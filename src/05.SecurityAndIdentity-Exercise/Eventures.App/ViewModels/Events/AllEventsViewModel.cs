0b0f54ab2ecb37e0350a0e7dd3fc89dd24bdf0c5
ShadyObeyd <obeyd.shady@gmail.com>
2018-11-28 12:34:08 +0200

    Added 05.SecurityAndIdentity-Exercise

---
+++ src/05.SecurityAndIdentity-Exercise/Eventures.App/ViewModels/Events/AllEventsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Eventures.App.ViewModels.Events
+{
+    public class AllEventsViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public string Place { get; set; }
+
+        public string StartToStr => this.Start.ToString("dd-MMM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+
+        public string EndToStr => this.End.ToString("dd-MMM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+    }
+}
